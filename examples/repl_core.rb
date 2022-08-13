307fe91dfd9d820971b7ff6ccd98860051d6c901
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-08-13 22:39:46 +0900

    Add: ラッパー版サンプルを追加

--- examples/repl_core.rb
+++ examples/repl_core.rb
@@ -1,10 +1,12 @@
 require "voicevox"
 require "reline"
 
-# FIXME: MacやLinuxでも製品版ボイボのpyopenjtalkのパスを指定するようにする
-dict_path = ENV.fetch("OPENJTALK_DICT") { ENV["LOCALAPPDATA"] + "/programs/voicevox/pyopenjtalk/open_jtalk_dic_utf_8-1.11" }
+dict_path = ENV.fetch("OPENJTALK_DICT") {
+  Voicevox.voicevox_path ? Voicevox.voicevox_path + "/pyopenjtalk/open_jtalk_dic_utf_8-1.11" : raise("OPENJTALK_DICTを設定してください。")
+}
 print "== 初期化中... "
-Voicevox::Core.initialize(false, 4, true)
+Voicevox::Core.initialize(false, 4, false)
+Voicevox::Core.load_model 0
 
 Voicevox::Core.voicevox_load_openjtalk_dict(dict_path)
 puts "完了"
@@ -17,7 +19,7 @@ loop do
   size_ptr = FFI::MemoryPointer.new(:int)
   return_ptr = FFI::MemoryPointer.new(:pointer)
   Voicevox::Core.voicevox_tts(text, 0, size_ptr, return_ptr)
-  puts "完了：#{size_ptr.read_int}バイト、アドレス：#{return_ptr.read_pointer.address.to_s(16)}"
+  print "完了：#{size_ptr.read_int}バイト、アドレス：#{return_ptr.read_pointer.address.to_s(16)}"
   data_ptr = return_ptr.read_pointer
   data = data_ptr.read_string(size_ptr.read_int)
 
@@ -26,4 +28,5 @@ loop do
 
   i += 1
   File.write("#{__dir__}/outputs/#{Process.pid}_#{i}.wav", data, mode: "wb")
+  puts "、#{Process.pid}_#{i}.wav"
 end
---
+++ examples/repl_wrapper.rb
@@ -0,0 +1,27 @@
+require "voicevox"
+require "reline"
+
+dict_path = ENV.fetch("OPENJTALK_DICT") {
+  Voicevox.voicevox_path ? Voicevox.voicevox_path + "/pyopenjtalk/open_jtalk_dic_utf_8-1.11" : raise("OPENJTALK_DICTを設定してください。")
+}
+print "== 初期化中... "
+vv = Voicevox.new
+vv.init(load_all_models: false)
+vv.load_openjtalk_dict(dict_path)
+character = Voicevox.characters[0].styles[0]
+character.load
+
+puts "完了"
+i = 0
+loop do
+  text = Reline.readline "> "
+  break unless text
+  print "生成中... "
+
+  data = vv.tts(text, character)
+  print "完了：#{data.bytesize}バイト"
+
+  i += 1
+  File.write("#{__dir__}/outputs/#{Process.pid}_#{i}.wav", data, mode: "wb")
+  puts "、#{Process.pid}_#{i}.wav"
+end
