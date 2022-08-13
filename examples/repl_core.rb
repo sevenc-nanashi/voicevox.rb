437a025d5b519fed3917c2a4d0d079e8990a0c89
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-08-13 17:37:13 +0900

    Change: REPLの出力を変更

--- examples/repl_core.rb
+++ examples/repl_core.rb
@@ -17,6 +17,7 @@ loop do
   size_ptr = FFI::MemoryPointer.new(:int)
   return_ptr = FFI::MemoryPointer.new(:pointer)
   Voicevox::Core.voicevox_tts(text, 0, size_ptr, return_ptr)
+  puts "完了：#{size_ptr.read_int}バイト、アドレス：#{return_ptr.read_pointer.address.to_s(16)}"
   data_ptr = return_ptr.read_pointer
   data = data_ptr.read_string(size_ptr.read_int)
 
@@ -25,5 +26,4 @@ loop do
 
   i += 1
   File.write("#{__dir__}/outputs/#{Process.pid}_#{i}.wav", data, mode: "wb")
-  puts "完了"
 end
