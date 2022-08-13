49bc8d028e1c853426bd85860f103fb5cade4d2d
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-08-13 15:13:17 +0900

    Add: coreの大部分を実装

--- lib/voicevox.rb
+++ lib/voicevox.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 require_relative "voicevox/version"
+require_relative "voicevox/core"
 
 module Voicevox
   class Error < StandardError; end
+
   # Your code goes here...
 end
