7ca3ad747f5951ba5121029d483c8869a8160881
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2023-01-26 23:17:00 +0900

    Add: エラーメッセージを追加

--- lib/voicevox/core.rb
+++ lib/voicevox/core.rb
@@ -149,6 +149,7 @@ class Voicevox
                     [:voicevox_result_code],
                     :string
   rescue LoadError
+    $e1 = $ERROR_INFO
     module Old
       extend FFI::Library
       ffi_lib %w[core.dll libcore.dylib libcore.so]
@@ -236,6 +237,16 @@ class Voicevox
       attach_function :voicevox_error_result_to_message,
                       [:voicevox_result_code],
                       :string
+    rescue LoadError => e2
+      raise(
+        LoadError,
+        "Failed to load voicevox_core! " +
+          "(voicevox_core.dll, libvoicevox_core.so, libvoicevox_core.dylib, " +
+          "core.dll, libcore.so, libcore.dylib)\n" +
+          "Make sure you have installed voicevox_core and its dependencies " +
+          "(such as onnxruntime), and that the voicevox_core shared library " +
+          "can be found in your library path."
+      )
     end
 
     module_function
@@ -428,5 +439,9 @@ class Voicevox
       options[:kana] = false
       options
     end
+    warn (
+           "Failed to load new core (voicevox_core.dll, libvoicevox_core.so, libvoicevox_core.dylib), " +
+             "using old core (core.dll, libcore.so, libcore.dylib)."
+         )
   end
 end
